1088c734b1fd21aa053446a00839a852781a5b23
garatquent <garatquent@eisti.eu>
2024-01-26 16:44:17 +0100

    add formation

---
+++ src/app/models/iformation.ts
@@ -0,0 +1,6 @@
+export interface IFormation {
+    structure:string,
+    desc:string,
+    date:string,
+    location:string
+}
--- src/app/pages/formation/formation.component.ts
+++ src/app/pages/formation/formation.component.ts
@@ -1,13 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { IFormation } from '../../models/iformation';
+import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-formation',
   standalone: true,
-  imports: [RouterModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './formation.component.html',
   styleUrl: './formation.component.css'
 })
-export class FormationComponent {
+export class FormationComponent implements OnInit{
+  listFormation: Array<IFormation> = [];
+  url: string = 'assets/data/formation.json';
 
+  constructor(private http:HttpClient) {}
+
+  ngOnInit() {
+    this.http.get(this.url).subscribe(res => {
+      this.listFormation = <Array<IFormation>>res;
+    })
+  }
 }
