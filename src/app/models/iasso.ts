56fafa51ca49b53357466d795f10f0da3c93c67b
garatquent <60227666+garatquent@users.noreply.github.com>
2024-01-26 11:30:23 +0000

    Deploying to gh-pages from @ garatquent/portfolio@6a5157bfb66bfae90d2133da081ef57f85ec5751 🚀

--- src/app/models/iasso.ts
+++
@@ -1,7 +0,0 @@
-export interface IAsso {
-    name: string,
-    role: string,
-    desc: string,
-    date: string,
-    location: string
-}
--- src/app/models/ixp-pro.ts
+++
@@ -1,8 +0,0 @@
-export interface IXpPro {
-    type:string,
-    title:string,
-    desc:string,
-    date:string,
-    company:string,
-    location:string
-}
